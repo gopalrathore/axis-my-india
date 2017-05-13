4359ce0f922fd35572b1b1d979cbf28af2cfb560
gopal rathore <gopalrathore94@gmail.com>
2017-05-13 13:39:01 +0530

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $('select').on('change', function() {
+    if (this.value == "others") {
+      $(this).next('input').removeAttr('disabled');
+      $(this).next('input').focus();
+    } else {
+      $(this).next('input').val("");
+      $(this).next('input').attr('disabled', 'disabled');
+    }
+
+  });
+});
